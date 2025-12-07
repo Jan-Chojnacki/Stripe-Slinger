a294b348bf73e90678708786cddbffe4da449a28
Jan Chojnacki <jan@chojnacki.dev>
2025-12-07 13:19:26 +0100

    Revert "fix?"
    This reverts commit d76c50e7f1f609a928d5dc0315f7971fb2d39db1.

--- crates/raid-rs/src/layout/stripe/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod raid0;
-pub mod raid1;
-pub mod raid3;
-pub mod traits;
--- crates/raid-rs/src/layout/stripe/raid1/raid1_tests.rs
+++
@@ -1,9 +0,0 @@
-use crate::layout::stripe::raid1::RAID1;
-
-#[test]
-fn zero_initializes_all_drives() {
-    let r = RAID1::<3, 4>::zero();
-    for d in 0..3 {
-        assert_eq!(r.0[d].as_bytes(), &[0u8; 4], "drive {}", d);
-    }
-}
